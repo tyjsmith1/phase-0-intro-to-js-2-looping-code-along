179838e922cf769df180960e3b1e2c1a504d227e
tylersmith <tyjsmith1@gmail.com>
2023-10-05 15:24:08 -0500

    looping results

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let cards = [];
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+
+    return cards;
+}
+
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown--);
+    }
+}
